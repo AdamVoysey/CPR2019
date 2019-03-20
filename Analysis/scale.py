5b0d21dcdf013372de68db180ebeb9c259afde3d
christophermaynard <christopher.maynard@metoffice.gov.uk>
2019-03-20 22:32:20 +0000

    Processing the scaling data.

--- Analysis/scale.py
+++ Analysis/scale.py
@@ -6,6 +6,33 @@ import matplotlib.ticker as ticker
 
 import sys
 
+def plot6Data(x, y1, y2, z1, z2, v1, v2):
+
+    fig, ax = plt.subplots()
+    xlim=[15,500]
+    ax.set_xlim(xlim[0],xlim[1])
+    width1=0.05*x
+
+    rects1 = ax.bar(0.85*x, y1, width1, color='b',label="HET1",edgecolor='black')
+    rects2 = ax.bar(0.9*x, y2, width1, color='r',label="HET6",edgecolor='black')
+    
+    rects3 = ax.bar(0.95*x, z1, width1, color='darkgreen',label="GST1",edgecolor='black')
+    rects4 = ax.bar(x, z2, width1, color='teal',label="GST6",edgecolor='black')
+
+    rects5 = ax.bar(1.05*x, v1, width1, color='purple',label="UT1",edgecolor='black')
+    rects6 = ax.bar(1.1*x, v2, width1, color='pink',label="UT6",edgecolor='black')
+
+    ax.set_xscale('log')
+    ax.minorticks_on()
+    ax.set_xticks(x)
+    ax.get_xaxis().set_major_formatter(ticker.ScalarFormatter())
+    ax.get_xaxis().set_minor_locator(ticker.NullLocator())
+
+    ax.set_xlabel("# nodes")
+    ax.set_ylabel("time (s)")
+    plt.show()
+    
+    
 def plot2Data(x, y1, y2):
 
     fig, ax = plt.subplots()
@@ -48,13 +75,30 @@ nodes=np.array(data[0:5,0])
 print nodes
 T6W=np.array(data[0:5,1])
 T1W=np.array(data[5:10,1])
-print T6W
-print T1W
+
+P6HE=np.array(data[0:5,2])
+P1HE=np.array(data[5:10,2])
+
+P6GS=np.array(data[0:5,3])
+P1GS=np.array(data[5:10,3])
+
+P6U=np.array(data[0:5,4])
+P1U=np.array(data[5:10,4])
+
+T6HE=T6W*P6HE
+T1HE=T1W*P1HE
+
+T6GS=T6W*P6GS
+T1GS=T1W*P1GS
+
+T6U=T6W*P6U
+T1U=T1W*P1U
 
 
 #nodes=np.array(sdata[:,0])
 #mpidat=np.array(sdata[:,1])
 #ompdat=np.array(sdata[:,2])
 
-plot2Data(nodes, T1W, T6W)
+#plot2Data(nodes, T1W, T6W)
 
+plot6Data(nodes, T1HE, T6HE, T1GS, T6GS, T1U, T6U)
