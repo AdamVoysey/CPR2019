b332f87ba83b6cefb1c53c79bb13cd0fe741ff15
christophermaynard <christopher.maynard@metoffice.gov.uk>
2019-03-20 19:29:10 +0000

    Started scaling section.

--- Analysis/scale-example.py
+++ Analysis/scale-example.py
@@ -11,12 +11,14 @@ def plot2Data(x, y1, y2):
     fig, ax = plt.subplots()
     xlim=[15,500]
     ax.set_xlim(xlim[0],xlim[1])
+    psy = [2800,175]
+    psx = [24,384]
     width1=0.1*x
     width2=0.1*x
-    rects1 = ax.bar(0.95*x, y1, width1, color='firebrick',label="MPI",edgecolor='black')
-    rects2 = ax.bar(1.05*x, y2, width2, color='goldenrod',label="OMP",edgecolor='black')
+    rects1 = ax.bar(0.95*x, y1, width1, color='teal',label="MPI",edgecolor='black')
+    rects2 = ax.bar(1.05*x, y2, width2, color='navy',label="OMP",edgecolor='black')
 #    l1 = ax.plot(psx,psy,color='black',linestyle='dashed',label="perfect scaling")
-    ax.legend(loc='upper right')
+    ax.legend(loc='upper left')
     ax.set_xscale('log')
 #    ax.set_yscale('log')
 
@@ -34,27 +36,19 @@ def plot2Data(x, y1, y2):
     ax.set_ylabel("time (s)")
 
 #    fstem="strong-scale"
-    fstem="wc-scale"
-    fname=fstem+".png"
-    plt.savefig(fname,format="png")
-    fname=fstem+".eps"
-    plt.savefig(fname,format="eps")    
+#    fstem="weak-scale"
+#    fname=fstem+".png"
+#    plt.savefig(fname,format="png")
+#    fname=fstem+".eps"
+#    plt.savefig(fname,format="eps")    
     plt.show()
 
 
-fname="perf.dat"
-data=np.genfromtxt(fname, skip_header=1, usecols=range(0,7))
-nodes=np.array(data[0:5,0])
-print nodes
-T6W=np.array(data[0:5,1])
-T1W=np.array(data[5:10,1])
-print T6W
-print T1W
+lname="weak.dat"
+sdata=np.genfromtxt(lname, skip_header=1, usecols=range(0,3))
+nodes=np.array(sdata[:,0])
+mpidat=np.array(sdata[:,1])
+ompdat=np.array(sdata[:,2])
 
-
-#nodes=np.array(sdata[:,0])
-#mpidat=np.array(sdata[:,1])
-#ompdat=np.array(sdata[:,2])
-
-plot2Data(nodes, T1W, T6W)
+plot2Data(nodes, mpidat, ompdat)
 
